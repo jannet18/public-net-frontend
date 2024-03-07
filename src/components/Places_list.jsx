8157a8b84a1e9ff5680dffc9dcd0c5ffa3977ec8
jannet18 <janetngei18@gmail.com>
2024-03-08 00:08:35 +0300

    places list table

--- src/components/Places_list.jsx
+++ src/components/Places_list.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useGetPublicPlacesQuery } from "../state/Api";
 
 const Places_list = () => {
@@ -8,8 +8,47 @@ const Places_list = () => {
   // loaded places : [] => get filled
   const { data } = useGetPublicPlacesQuery();
   console.log(data);
+  // const [placesData] = useMemo(() => {
+  //   if (!data) return [];
 
-  return <div>Places_list</div>;
+  //   const totalPlaces = {
+  //     id: "places",
+  //     data: [],
+  //   };
+  //   Object.values(data ?? []).map((place, index) => {
+  //     totalPlaces.data = [...totalPlaces.data];
+  //   });
+  //   const placesData = [totalPlaces];
+  //   console.log(placesData);
+  //   return placesData;
+  // }, [data]);
+
+  return (
+    <div>
+      {data &&
+        data.places.map((place, index) => (
+          <table key={index}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>City</th>
+                <th>Recent Upload Speed</th>
+                <th>Recent Upload Speed Units</th>
+                <th>Number of measurements</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{place.name}</td>
+                <td>{place.city}</td>
+                <td>{place.most_recent_download_speed}</td>
+                <td>{place.most_recent_download_units}</td>
+              </tr>
+            </tbody>
+          </table>
+        ))}
+    </div>
+  );
 };
 
 export default Places_list;
