4ab30a48d658d22c9124525e9c4777c9c9a1183e
jannet18 <janetngei18@gmail.com>
2024-03-13 22:18:26 +0300

    installing conic gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Places_list from "./components/Places_list";
 import Home from "./pages/Home.jsx";
+import InternetSpeedsNew from "./components/InternetSpeedsNew.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/places" element={<Places_list />} />
+        <Route path="/new" element={<InternetSpeedsNew />} />
       </Routes>
     </>
   );
---
+++ src/components/InternetSpeedsNew.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const InternetSpeedsNew = () => {
+  return (
+    // <div className="bg-[linear-gradient(45deg,_indigo-950,_to-blue-900)]">
+    <>
+      <div className="w-full h-full text-white inline-block text-center bg-[#223140] bg-gradient-to-b from-[transparent 50%] to-rgba(255,255,255,0.08) rounded-md p-6 relative top-[50%] left-[50%] transform translate-[-50%, -50%]">
+        <div className="w-[360px] h-[420px]">
+          <h1 className="text-[rgba(1, 182, 190, 0.3)_0%,_transparent_65%]">
+            Check Location Internet Speed
+          </h1>
+          <span className="inline-block w-[240px] h-[240px] rounded-[50%] bg-[conic-gradient(rgba(1,_182,_190,_0.3)_0%,_transparent_65%)]"></span>
+          {/* <span className=" inline-block w-[240px] h-[240px] rounded-full bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-rgba(1, 182, 190, 0.3) 0% to-[transparendt 65%])"></span> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InternetSpeedsNew;
